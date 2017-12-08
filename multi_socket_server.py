b2bd9f90e40d77c9370cb55b2c285e55e50698f2
Ashik Mahmud <ashikmahmud137@gmail.com>
2017-12-09 00:57:19 +0600

    First Commit

---
+++ multi_socket_server.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target=self.listenToClient, args=(client, address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data
+                    response = data
+                    Light_ON_OFF(response.decode())
+                    client.send(response)
+            except:
+                client.close()
+                return False
+
+
+def Light_ON_OFF(text):
+    if text == 'on':
+        print('Light ON')
+    elif text == 'off':
+        print('Light OFF')
+
+
+if __name__ == "__main__":
+    while True:
+        port_num = input("Port? ")
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('', port_num).listen()
